0f216a94bba300351333e200d4ad3099b3325396
Zhi Heng <yapzhhg@gmail.com>
2023-11-05 16:07:53 +0800

    Update KBD67Lite keymap to QMK 0.22.14

--- kbd67lite/README.md
+++ kbd67lite/README.md
@@ -1,5 +1,6 @@
 # Custom KBD67 Lite R1 keymap
 
+- Base firmware: `kbdfans/kbd67/mkiirgb/v2`
 - Includes TaUnTcAsE (toggle with FN + CapsLck)
 - Tapdancing for easier access to F-row keys (Tap left Alt + `1 to =` to input `F1 to F12`)
-  - Tap, release and then hold left Alt before pressing `1 to =` to input `Alt + F1 to F12`)
+  - Tap, release and then hold left Alt before pressing `1 to =` to input `Alt + F1 to F12`
--- kbd67lite/keymap.c
+++ kbd67lite/keymap.c
@@ -15,7 +15,8 @@ enum {
 };
 
 enum {
-  ALT_OSL1 = 0
+  ALT_OSL1 = 0,
+  TD_GESC = 1,
 };
 
 enum dumb_keycodes {
@@ -24,27 +25,27 @@ enum dumb_keycodes {
 
 bool tAuNtTeXt = false;
 
-int cur_dance (qk_tap_dance_state_t *state);
-void alt_finished (qk_tap_dance_state_t *state, void *user_data);
-void alt_reset (qk_tap_dance_state_t *state, void *user_data);
+int cur_dance (tap_dance_state_t *state);
+void alt_finished (tap_dance_state_t *state, void *user_data);
+void alt_reset (tap_dance_state_t *state, void *user_data);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		[0] = LAYOUT_65_ansi_blocker( 
-			KC_GESC, KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,   KC_HOME,
-		    KC_TAB,  KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC, KC_BSLASH, KC_PGUP,
+			TD(TD_GESC), KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,    KC_7,  KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,  KC_BSPC,   KC_HOME,
+		    KC_TAB,  KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,    KC_U,  KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS, KC_PGUP,
 			KC_CAPS, KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,    KC_J,  KC_K,    KC_L,     KC_SCLN,  KC_QUOT,           KC_ENT,    KC_PGDN,
 		    KC_LSFT, KC_Z,     KC_X,     KC_C,  KC_V,  KC_B,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,           KC_UP,     KC_END,
-		    KC_LCTL, KC_LGUI,  TD(ALT_OSL1),           KC_SPC,                MO(1),           KC_RALT,    KC_LEFT,           KC_DOWN,   KC_RIGHT),
+		    KC_LCTL, KC_LGUI,  TD(ALT_OSL1),           KC_SPC,                         MO(1),    KC_RALT,    KC_LEFT,           KC_DOWN,   KC_RIGHT),
 		[1] = LAYOUT_65_ansi_blocker( 
-			KC_GESC,       KC_F1,    KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,   KC_INS,
-		    KC_TRNS,       RGB_TOG,  RGB_MOD, RGB_HUI,RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_SLCK,  KC_PAUS, RESET,    KC_PGUP,
-			TAUNTTXT,       RGB_SPI,  RGB_SPD, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          EEP_RST,  KC_PGDN,
+			KC_GRV,       KC_F1,    KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,   KC_INS,
+		    KC_TRNS,       RGB_TOG,  RGB_MOD, RGB_HUI,RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_SCRL,  KC_PAUS, QK_BOOT,  KC_PGUP,
+			TAUNTTXT,      RGB_SPI,  RGB_SPD, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          EE_CLR,   KC_PGDN,
 		    KC_LSFT,       KC_TRNS,  KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          KC_VOLU,  KC_MUTE,
 		    KC_TRNS,       KC_TRNS,  TD(ALT_OSL1),             KC_TRNS,                   KC_TRNS,          KC_TRNS,  KC_MPRV,          KC_VOLD,  KC_MNXT),
 };
 
 
-int cur_dance (qk_tap_dance_state_t *state) {
+int cur_dance (tap_dance_state_t *state) {
   if (state->count == 1) {
     if (state->pressed) return SINGLE_HOLD;
     else return SINGLE_TAP;
@@ -65,7 +66,7 @@ static tap alttap_state = {
   .state = 0
 };
 
-void alt_finished (qk_tap_dance_state_t *state, void *user_data) {
+void alt_finished (tap_dance_state_t *state, void *user_data) {
   alttap_state.state = cur_dance(state);
   switch (alttap_state.state) {
     case SINGLE_TAP: set_oneshot_layer(1, ONESHOT_START); clear_oneshot_layer_state(ONESHOT_PRESSED); break;
@@ -78,7 +79,7 @@ void alt_finished (qk_tap_dance_state_t *state, void *user_data) {
   }
 }
 
-void alt_reset (qk_tap_dance_state_t *state, void *user_data) {
+void alt_reset (tap_dance_state_t *state, void *user_data) {
   switch (alttap_state.state) {
     case SINGLE_TAP: break;
     case SINGLE_HOLD: unregister_code(KC_LALT); break;
@@ -88,8 +89,10 @@ void alt_reset (qk_tap_dance_state_t *state, void *user_data) {
   alttap_state.state = 0;
 }
 
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [ALT_OSL1]     = ACTION_TAP_DANCE_FN_ADVANCED(NULL,alt_finished, alt_reset)
+tap_dance_action_t tap_dance_actions[] = {
+  [ALT_OSL1]     = ACTION_TAP_DANCE_FN_ADVANCED(NULL, alt_finished, alt_reset),
+  // TODO gotta figure out why this isn't working
+  [TD_GESC] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_GRV),
 };
 
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
@@ -100,7 +103,7 @@ bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed && is_oneshot_layer_active())
       clear_oneshot_layer_state(ONESHOT_OTHER_KEY_PRESSED);
       return true;
-    case RESET:
+    case QK_BOOT:
       /* Don't allow reset from oneshot layer state */
       if (record->event.pressed && is_oneshot_layer_active()){
         clear_oneshot_layer_state(ONESHOT_OTHER_KEY_PRESSED);
